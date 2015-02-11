4fe2768a4642eb3a852c2bcfdc58e64f13d49bd2
dextacy10-13 <jamesmyersone@hotmail.com>
2015-02-11 01:51:45 +0000

    edit index.php with some sample URLS

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 include('app/config.php');
 ?>
 <h1>JIM-Tervals</h1>
+<p>Some example pages that poll the Intervals API</p>
 <nav>
     <ul>
-        <li><a href="milestone.php">Milestones</a></li>
+        <li><a href="milestone.php">Generate Milestone Task Spreadsheets</a></li>
+        <li><a href="taskOpen.php?excludeclosed=1&limit=20">Display 20 Open Tasks</a></li>
     </ul>
 </nav>
